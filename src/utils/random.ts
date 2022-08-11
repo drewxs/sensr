7041b4eeaa13f663d4d9949e162c91f30d51d0d4
kito0 <drewshah0@gmail.com>
2022-08-10 22:07:58 -0600

    feat: create xavier weight initialization

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -19,3 +19,13 @@ export const random = (min: number = 0, max: number): number => {
 export const randomInt = (min: number = 0, max: number): number => {
   return Math.floor(Math.random() * (max - min) + min);
 };
+
+/**
+ * Xavier weight initialization, return a random number with a uniform probability distribution.
+ *
+ * @param n - number of inputs
+ * @returns random number
+ */
+export const xavier = (n: number): number => {
+  return random(-(1 / Math.sqrt(n)), 1 / Math.sqrt(n));
+};
